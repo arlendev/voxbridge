c2b0913a4e4df585079c8340e99445c6166b7151
Arlen Possamai <77725374+arlendev@users.noreply.github.com>
2025-02-13 21:07:16 +0100

    Update reconhece-voz.js

--- frontend/assets/js/reconhece-voz.js
+++ frontend/assets/js/reconhece-voz.js
@@ -127,7 +127,7 @@ btnSalvar.addEventListener("click", () => {
 
     console.log("📤 Enviando dados para o backend:", JSON.stringify(dados, null, 2));
 
-    fetch("http://localhost:3000/notas", {
+    fetch("https://voxbridge-backend.onrender.com/notas", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dados)
